d7fe7d456472b12bd38f1f21704a2a584c9b622e
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-09 14:16:10 -0500

    Refactor build and run scripts

---
+++ script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+action=$1
+
+
+if [ "$action" = "install" ]; then
+
+    # Navigate to docs_skeleton directory
+    cd ./docs/docs_skeleton
+
+    # Install Yarn
+    npm install -g yarn
+
+    # Install Node.js dependencies
+    npx yarn install --frozen-lockfile --non-interactive
+
+    # Navigate back to root directory
+    cd ../..
+
+
+elif [ "$action" = "run" ]; then
+
+    # Navigate to docs_skeleton directory
+    cd ./docs/docs_skeleton
+
+    # Create a docs folder
+    mkdir -p ./docs
+
+    # Copy markdown and notebook files to docs_skeleton
+    cp -r ../files/** ./docs/
+
+    # Run nbdoc
+    nbdoc_build
+
+    # run npm run serve
+    npm run start
+
+    # remove docs folder
+    rm -rf ./docs
+
+    # Navigate back to root directory
+    cd ../..
+
+else
+    echo "Invalid action"
+fi
