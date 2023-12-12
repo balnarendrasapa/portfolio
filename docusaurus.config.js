94f5e5af085bc65f1b857605010773dd577df2c1
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-12 15:03:43 -0500

    Update image path in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'Header-Footer Files/Icon.svg',
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
