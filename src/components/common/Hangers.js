c9c2d468b940517feb66ebc5e8bef441b2104ffc
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-25 17:55:05 -0500

    add svg icons

--- src/components/common/Hangers.js
+++ src/components/common/Hangers.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import './Hangers.css'
+import Linkedin from '@site/static/social-media/linkedin.svg'
 
 const Hangers = () => {
   return (
     <>
+      <div className="RightLineStyle" />
       <div className="RightHangerStyle">
         <div>bnsapa2000@gmail.com</div>
       </div>
+      <div className="LeftLineStyle" />
       <div className="LeftHangerStyle">
-        <div>leftHanger</div>
+        <div>
+          <Linkedin  style={{
+            width: '30px',
+            height: '30px',
+            marginRight: '5px',
+            verticalAlign: 'middle',
+            transform: 'rotate(-90deg)',
+          }}/>
+          <Linkedin  style={{
+            width: '30px',
+            height: '30px',
+            marginRight: '5px',
+            verticalAlign: 'middle',
+            transform: 'rotate(-90deg)',
+          }}/>
+        </div>
       </div>
     </>
   )
