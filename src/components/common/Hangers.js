33df6f8f5331b55b2c48aa5bd59e3969c605cfae
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-12-25 16:58:10 -0500

    add hangers.js

---
+++ src/components/common/Hangers.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hangers.css'
+
+const Hangers = () => {
+  return (
+    <>
+      <div className="RightHangerStyle">
+        <div>bnsapa2000@gmail.com</div>
+      </div>
+      <div className="LeftHangerStyle">
+        <div>leftHanger</div>
+      </div>
+    </>
+  )
+}
+
+export default Hangers
