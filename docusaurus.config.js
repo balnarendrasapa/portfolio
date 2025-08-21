215fbddeaeb403c3d905a7231b79767731c8bd4d
Bal Narendra <bnsapa2000@gmail.com>
2025-08-21 19:55:47 -0400

    Update intro.mdx and docusaurus.config.js for improved content and configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
         },
         docs: {
           routeBasePath: '/',
-          sidebarPath: './sidebars.js',
+          sidebarPath: false,
         },
         blog: false,
         theme: {
